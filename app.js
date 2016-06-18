b4a45268132966006b342cdb37563fd582db4434
James Loomis <James Loomis>
2016-06-18 03:12:14 -0400

    Added gulpfile

--- app.js
+++ app.js
@@ -1,46 +1,37 @@
-var express       = require('express');
-var http          = require('http');
-var path          = require('path');
-var config        = require('./config')();
-var app           = express();
+var express = require('express');
+var http = require('http');
+var path = require('path');
+var config = require('./config')();
+var app = express();
 
-var engine        = require('tingodb');
-var tungus        = require('tungus');
-var mongoose      = require('mongoose');
+var engine = require('tingodb');
+var tungus = require('tungus');
+var mongoose = require('mongoose');
 
-var favicon       = require('serve-favicon');
-var logger        = require('morgan');
-var session       = require('express-session');
-var cookieParser  = require('cookie-parser');
-var bodyParser    = require('body-parser');
-var debug         = require('debug')('app4');
-var errorHandler  = require('errorhandler');
-
-
-var routes        = require('./controllers/index');
-var users         = require('./controllers/users');
-var Admin         = require('./controllers/Admin');
-
-
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var session = require('express-session');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var debug = require('debug')('app4');
+var errorHandler = require('errorhandler');
 
 // All environments
 
-// view engine setup
-app.set('views', path.join(__dirname, 'templates'));
-app.set('view engine', 'jade');
-
 app.use(favicon(path.join(__dirname, 'public', 'dragon.ico')));
 app.use(logger('dev'));
-app.use(session({ resave: true,
-                  saveUninitialized: true,
-                  secret: 'uwotm8' }));
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: 'uwotm8'
+}));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
 
 // error handlers
 
@@ -52,7 +43,7 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
@@ -60,20 +51,20 @@ app.use(function(err, req, res, next) {
   });
 });
 
-mongoose.connect('tingodb://' + __dirname + '/data', function(err, db) {
-  if(err) {
+mongoose.connect('tingodb://' + __dirname + '/data', function (err, db) {
+  if (err) {
     console.log('Cannot connect to database');
   } else {
-    var attachDB = function(req, res, next) {
-        req.db = db;
-        next();
+    var attachDB = function (req, res, next) {
+      req.db = db;
+      next();
     };
 
-    app.all('/admin*', attachDB, function(req, res, next){
+    app.all('/admin*', attachDB, function (req, res, next) {
       Admin.run(req, res, next);
     });
 
-    app.all('/', attachDB, function(req, res, next){
+    app.all('/', attachDB, function (req, res, next) {
       Home.run(req, res, next);
     });
 
@@ -81,8 +72,8 @@ mongoose.connect('tingodb://' + __dirname + '/data', function(err, db) {
   }
 });
 
-var server = http.createServer(app).listen(config.port, function(){
-    debug('Tapp Express server listening on port ' + server.address().port)
+var server = http.createServer(app).listen(config.port, function () {
+  debug('Tapp Express server listening on port ' + server.address().port)
 });
 
 module.exports = server
